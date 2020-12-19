bdbc5627886209cca5929bd4dfc0287f4074e3be
d-otis <dan@dan-foley.com>
2020-12-19 15:57:59 -0500

    define sendMail()

--- mailer.js
+++ mailer.js
@@ -10,12 +10,35 @@ const mail = nodemailer.createTransport({
   }
 })
 
-const mailOptions = {
-  from: process.env.GMAIL_USR_FROM,
-  to: process.env.GMAIL_USR_TO,
-  subject: `Test Subject @ ${Date.now()}`,
-  text: 'i"m oing a crossword puzzle'
-}
+// const mailOptions = {
+//   from: process.env.GMAIL_USR_FROM,
+//   to: process.env.GMAIL_USR_TO,
+//   subject: `Test Subject @ ${Date.now()}`,
+//   text: 'i"m oing a crossword puzzle'
+// }
+
+// mail.sendMail(mailOptions, (err, info) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log('Email sent: ' + info.response)
+//   }
+// })
+
+let mailedTweets = []
+
+const sendMail = tweets => {
+  const formattedTweets = tweetArr => {
+    let body = ''
+    for (const tweet of tweetArr) {
+      console.log(`Checking tweet# ${tweet.id}`)
+      if (!mailedTweets.includes(tweet.id)) {
+        body = body.concat(tweet.created_at + "\r\n" + tweet.text + "\r\n\r\n")
+        mailedTweets.push(tweet.id)
+      }
+    }
+    return body
+  }
 
 mail.sendMail(mailOptions, (err, info) => {
   if (err) {
