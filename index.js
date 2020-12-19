c8d18b6c9454820dfc71f7bf293fc525eba412c5
d-otis <dan@dan-foley.com>
2020-12-19 09:54:32 -0500

    add Twitter docs boilerplate

--- index.js
+++ index.js
@@ -1,2 +1,67 @@
-import fetch from 'node-fetch'
-const nodemailer = require('nodemailer')
+// const nodemailer = require('nodemailer')
+const needle = require('needle');
+// const dotenv = require('dotenv').config();
+const dotenvMe = require('./env.util')
+
+const userId = 117424097;
+const url = `https://api.twitter.com/2/users/${userId}/tweets`;
+const bearerToken = process.env.BEARER_TOKEN;
+
+const getUserTweets = async () => {
+    let userTweets = [];
+    let username
+    let params = {
+        "max_results": 10,
+        "tweet.fields": "created_at",
+        "expansions": "author_id"
+    }
+
+    const options = {
+        headers: {
+            "authorization": `Bearer ${bearerToken}`
+        }
+    }
+
+    let hasNextPage = true;
+    let nextToken = null;
+    console.log("Retrieving Tweets...");
+    while (hasNextPage) {
+        let resp = await getPage(params, options, nextToken);
+        if (resp && resp.meta && resp.meta.result_count && resp.meta.result_count > 0) {
+          // console.log('username: ', resp.includes.users.username)
+            if (resp.data) {
+                userTweets.push.apply(userTweets, resp.data);
+            }
+            username = resp.includes.users[0].username
+            if (resp.meta.next_token) {
+                nextToken = resp.meta.next_token;
+            }
+        } else {
+            hasNextPage = false;
+        }
+    }
+
+    console.log(userTweets);
+    console.log(`Got ${userTweets.length} Tweets from ${username}!`);
+
+}
+
+const getPage = async (params, options, nextToken) => {
+    if (nextToken) {
+        params.next_token = nextToken;
+    }
+
+    try {
+        const resp = await needle('get', url, params, options);
+
+        if (resp.statusCode != 200) {
+            console.log(`${resp.statusCode} ${resp.statusMessage}:\n${resp.body}`);
+            return;
+        }
+        return resp.body;
+    } catch (err) {
+        throw new Error(`Request failed: ${err}`);
+    }
+}
+
+getUserTweets();
