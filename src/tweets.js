7ad10a1c8d84a9a89e2a461d373ebbc6eb80fa51
d-otis <dan@dan-foley.com>
2020-12-23 14:45:22 -0500

    indentation fixes

--- src/tweets.js
+++ src/tweets.js
@@ -20,46 +20,43 @@ const getUserTweets = async () => {
   }
 
   const options = {
-      headers: {
-          "authorization": `Bearer ${bearerToken}`
-      }
+    headers: {
+      "authorization": `Bearer ${bearerToken}`
+    }
   }
 
   let hasNextPage = true;
   let nextToken = null;
   console.log("Retrieving Tweets...");
   while (hasNextPage) {
-      let resp = await getPage(params, options, nextToken);
-      if (resp && resp.meta && resp.meta.result_count && resp.meta.result_count > 0) {
-        // console.log('username: ', resp.includes.users.username)
-          if (resp.data) {
-              userTweets.push.apply(userTweets, resp.data);
-          }
-          username = resp.includes.users[0].username
-          if (resp.meta.next_token) {
-              nextToken = resp.meta.next_token;
-          }
+    let resp = await getPage(params, options, nextToken);
+    if (resp && resp.meta && resp.meta.result_count && resp.meta.result_count > 0) {
+      if (resp.data) {
+        userTweets.push.apply(userTweets, resp.data);
+      }
+      username = resp.includes.users[0].username
+      if (resp.meta.next_token) {
+        nextToken = resp.meta.next_token;
       } else {
-          hasNextPage = false;
+        hasNextPage = false
       }
   }
 
   let matchingTweets = parseTweets(userTweets)
   let tweetQueue = []
   if (matchingTweets.length > 0) {
-      matchingTweets.forEach(tweet => {
-          if(!MAILED_TWEETS.includes(tweet.id)) {
-              tweetQueue.push(tweet)
-              MAILED_TWEETS.push(tweet.id)
-          }
-      })
-
-      if (tweetQueue.length > 0 ) {
-        sendMail(tweetQueue)
-        // sendSMS(tweetQueue)
-      } else {
-        console.log('no new relevant tweets')
+    matchingTweets.forEach(tweet => {
+      if (!MAILED_TWEETS.includes(tweet.id)) {
+        tweetQueue.push(tweet)
+        MAILED_TWEETS.push(tweet.id)
       }
+    })
+
+    if (tweetQueue.length > 0) {
+      sendMail(tweetQueue)
+    } else {
+      console.log('no new relevant tweets')
+    }
   }
 }
 
@@ -69,7 +66,7 @@ const getPage = async (params, options, nextToken) => {
   }
 
   try {
-      const resp = await needle('get', url, params, options);
+    const resp = await needle('get', url, params, options);
 
       if (resp.statusCode != 200) {
           console.log(`${resp.statusCode} ${resp.statusMessage}:\n${resp.body}`);
@@ -77,16 +74,16 @@ const getPage = async (params, options, nextToken) => {
       }
       return resp.body;
   } catch (err) {
-      throw new Error(`Request failed: ${err}`);
+    throw new Error(`Request failed: ${err}`);
   }
 }
 
 const parseTweets = tweetArray => {
   let parsedTweets = []
   tweetArray.filter(tweet => {
-      if(tweet.text.includes('resume') || tweet.text.includes('suspended')) {
-          parsedTweets.push(tweet)
-      }
+    if (tweet.text.includes('resume') || tweet.text.includes('suspended')) {
+      parsedTweets.push(tweet)
+    }
   })
   return parsedTweets
 }
