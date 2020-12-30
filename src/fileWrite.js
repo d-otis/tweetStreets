977f6fcd6dccbdb09e76779eb82906d506c0911d
d-otis <dan@dan-foley.com>
2020-12-30 12:18:46 -0500

    stub out persistIds function

--- src/fileWrite.js
+++ src/fileWrite.js
@@ -3,14 +3,16 @@ const fs = require('fs')
 let numArray = [12345, 678910, 75723874986]
 let stringArray = ["wefiuwwuenfwef", "2345876234523", "krsfbsfsdfg"]
 
-numArray.forEach(num => {
-  fs.appendFile('message.txt', num + "\n", (err) => {
-    if (err) throw err;
-    console.log(`"${num}" was appended to file!`);
-  });
-})
 
-// fs.appendFile('message.txt', 'data to append', (err) => {
-//   if (err) throw err;
-//   console.log('The "data to append" was appended to file!');
-// });
+const persistTweetIds = ids => {
+  ids.forEach(num => {
+    fs.writeFileSync('/db/data.txt', num + ",", (err) => {
+      if (err) throw err;
+      console.log(`"${num}" was appended to file!`);
+    });
+  })
+}
+
+// persistTweetIds(numArray)
+
+module.persistTweetIds = persistTweetIds
