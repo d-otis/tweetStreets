1df37f1aae0b498ea2d40b000dffafa7e5d1bbe6
d-otis <dan@dan-foley.com>
2020-12-30 17:16:00 -0500

    require formattedTweets() utility function

--- src/mailer.js
+++ src/mailer.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer')
 const dotenv = require('dotenv').config();
+const { formattedTweets } = require('./util')
 
 const mail = nodemailer.createTransport({
   service: 'gmail',
