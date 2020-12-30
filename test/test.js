7b5fa3e19062ce54ec8b07f601ed6d2b066f7f4d
d-otis <dan@dan-foley.com>
2020-12-30 18:34:09 -0500

    change equal to strictEqual

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var { formatDate, formattedTweets } = require('../src/util')
 describe('Array', function() {
   describe("#indexOf()", function () {
     it('should return -1 when the valie is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1)
+      assert.strictEqual([1, 2, 3].indexOf(4), -1)
     })
   })
 })
