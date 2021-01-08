f36ccb7c017c99832b16d01b05e31497ffa9db20
d-otis <dan@dan-foley.com>
2021-01-07 20:07:31 -0500

    whitespace

--- src/fileWrite.js
+++ src/fileWrite.js
@@ -1,18 +1,12 @@
 const fs = require('fs')
 
-let numArray = [12345, 678910, 75723874986]
-let stringArray = ["wefiuwwuenfwef", "2345876234523", "krsfbsfsdfg"]
-
-
 const persistTweetIds = ids => {
   ids.forEach(num => {
-    fs.writeFileSync('/db/data.txt', num + ",", (err) => {
+    fs.writeFile('/db/data.txt', num + ",", (err) => {
       if (err) throw err;
       console.log(`"${num}" was appended to file!`);
     });
   })
 }
-
-// persistTweetIds(numArray)
 
 module.persistTweetIds = persistTweetIds
