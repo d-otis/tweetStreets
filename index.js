51b2986cf6c675f3ec13fcd4a6cba30d32da5e72
d-otis <dan@dan-foley.com>
2021-01-12 20:44:24 -0500

    pass savedIds to getUserTweets()

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 const { getUserTweets } = require('./src/tweets')
 const savedIds = require('./src/savedIds')
 
-console.log('script starting')
-console.log()
-// initial fetch
-getUserTweets()
-
-let rawDate = Date.now()
-let date = new Date(rawDate)
-
-console.log(`Checked @ ${date.toString()}`)
-console.log()
-console.log('done')
-console.log()
-console.log('==================================')
+getUserTweets(savedIds)
