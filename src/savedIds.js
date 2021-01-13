8e06719667740f80c02ba9ddbc602965945609e4
d-otis <dan@dan-foley.com>
2021-01-12 19:48:28 -0500

    rename getStore.js -> src/savedIds.js

---
+++ src/savedIds.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const processData = data => {
+  return data.substr(0, data.length - 1).split(",")
+}
+
+const data = fs.readFileSync(`${process.cwd()}/db/ids.txt`, 'utf-8', (err, data) => {
+    if (data) console.log(data)
+    console.log('inside readFile')
+    processData(data)
+  })
+
+const savedIds = processData(data)
+
+module.savedIds = savedIds
