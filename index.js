37211e91a8fb714764b65b9504e5acdeeaa8d95b
d-otis <dan@dan-foley.com>
2021-01-13 19:44:21 -0500

    add Date/time printing for better log readability

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const { getUserTweets } = require('./src/tweets')
 const savedIds = require('./src/savedIds')
 
+const dateObj = new Date()
+const date = {
+  date: dateObj.toLocaleString().split(", ")[0],
+  time: dateObj.toLocaleString().split(", ")[1],
+}
+console.log()
+console.log()
+console.log("====================================")
+console.log("===Checking PhilaStreets' Twitter===")
+console.log(`=====${date.date} @ ${date.time}====`)
+console.log("====================================")
 getUserTweets(savedIds)
