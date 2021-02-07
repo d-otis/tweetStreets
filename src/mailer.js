d722fda448797b7b041dcd7d80da58d4e2c6a0db
d-otis <dan@dan-foley.com>
2021-02-06 19:30:30 -0500

    refactor dotenv config to explicitly reference local .env file via __dirname

--- src/mailer.js
+++ src/mailer.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer')
 const dotenv = require('dotenv').config();
+dotenv.config({ path: __dirname + '/../.env' })
 const { formattedTweets } = require('./util')
 
 const mail = nodemailer.createTransport({
