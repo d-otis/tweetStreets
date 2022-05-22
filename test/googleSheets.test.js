e2a716f65d02cf16bd286bedac68ec7ce129e2c5
d-otis <dan@danfoley.dev>
2022-05-22 15:38:00 -0400

    temporarily remove google sheets tests

--- test/googleSheets.test.js
+++ test/googleSheets.test.js
@@ -1,9 +1,9 @@
-const should = require("should");
+// const should = require("should");
 const { idsFromSheet, getEmailsFromSheet } = require("../src/googleSheets");
 
 describe("googleSheets Module", function () {
   describe("idsFromSheet()", function () {
-    it("should return array of tweet ids as Strings that saved in Google Sheets", async function () {
+    xit("should return array of tweet ids as Strings that saved in Google Sheets", async function () {
       const ids = await idsFromSheet();
       ids.should.be.instanceOf(Array);
       ids[0].should.have.length(19);
